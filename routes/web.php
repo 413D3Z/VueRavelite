0530bcc2360c81c8208b05b881b9dde2619eee9c
413D3Z <28901773+413D3Z@users.noreply.github.com>
2024-04-03 20:19:34 +0800

    Fix Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+use App\Http\Controllers\SpaController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('{path}', SpaController::class)->where('path', '(.*)');
